99662c6872e236d62e376e02f807f8d4257a83fb
Haseeb Majid <haseeb@joinzoe.com>
2022-07-16 14:33:30 +0100

    made some changes

--- fish/config/config.fish
+++ fish/config/config.fish
@@ -3,11 +3,15 @@ set -x PATH $PATH $HOME/.pyenv/bin $HOME/.poetry/bin $HOME/.local/bin
 set -gx PIPX_DEFAULT_PYTHON "$HOME/.pyenv/versions/3.10.5/bin/python"
 
 # The next line updates PATH for the Google Cloud SDK.
-if [ -f '/home/haseeb/google-cloud-sdk/path.fish.inc' ]; . '/home/haseeb/google-cloud-sdk/path.fish.inc'; end
+if [ -f "$HOME/google-cloud-sdk/path.fish.inc" ]
+    . "$HOME/google-cloud-sdk/path.fish.inc"
+end
 
 
 # pyenv init
 if command -v pyenv 1>/dev/null 2>&1
-  pyenv init - | source
+    pyenv init - | source
 end
+
 zoxide init fish | source
+source ~/.asdf/asdf.fish
