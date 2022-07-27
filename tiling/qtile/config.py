a8bbf3106d4fc4eb462ef97718d03d26825c907d
Haseeb Majid <hello@haseebmajid.dev>
2022-07-27 22:52:36 +0100

    test

--- tiling/qtile/config.py
+++ tiling/qtile/config.py
@@ -217,16 +217,16 @@ colors = [
     ["#d8dee9", "#d8dee9"],  # 1 foreground
     ["#3b4252", "#3b4252"],  # 2 background lighter
     ["#bf616a", "#bf616a"],  # 3 red
-    ["#a3be8c", "#a3be8c"],  # 4 green
-    ["#ebcb8b", "#ebcb8b"],  # 5 yellow
-    ["#81a1c1", "#81a1c1"],  # 6 blue
-    ["#b48ead", "#b48ead"],  # 7 magenta
+    ["#b4d273", "#b4d273"],  # 4 green
+    ["#e5b567", "#e5b567"],  # 5 yellow
+    ["#6c99bb", "#6c99bb"],  # 6 blue
+    ["#b05279", "#b05279"],  # 7 magenta
     ["#88c0d0", "#88c0d0"],  # 8 cyan
-    ["#e5e9f0", "#e5e9f0"],  # 9 white
+    ["#d6d6d6", "#d6d6d6"],  # 9 white
     ["#4c566a", "#4c566a"],  # 10 grey
-    ["#d08770", "#d08770"],  # 11 orange
+    ["#e87d3e", "#e87d3e"],  # 11 orange
     ["#8fbcbb", "#8fbcbb"],  # 12 super cyan
-    ["#5e81ac", "#5e81ac"],  # 13 super blue
+    ["#9e86c8", "#9e86c8"],  # 13 super blue
     ["#242831", "#242831"],  # 14 super dark background
 ]
 
