781b339ccff855260fe61566c69eaa369ba2cf17
Haseeb Majid <hello@haseebmajid.dev>
2024-03-17 04:57:00 +0000

    fix m

--- hosts/vm/configuration.nix
+++ hosts/vm/configuration.nix
@@ -5,14 +5,13 @@
 }: {
   imports = [
     ./hardware-configuration.nix
-    ./disks.nix
 
     ../../nixos
     ../../nixos/users/haseeb.nix
   ];
 
   networking = {
-    hostName = "desktop";
+    hostName = "vm";
   };
 
   modules.nixos = {
@@ -21,24 +20,16 @@
     docker.enable = true;
   };
 
-  swapDevices = [{device = "/swap/swapfile";}];
-
   services.xserver.enable = true;
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.desktopManager.gnome.enable = true;
   services.qemuGuest.enable = true;
   services.spice-vdagentd.enable = true;
 
-  boot.initrd.systemd.enable = true;
-  boot = {
-    supportedFilesystems = lib.mkForce ["btrfs"];
-    kernelPackages = pkgs.linuxPackages_latest;
-    loader = {
-      systemd-boot.enable = true;
-      efi.canTouchEfiVariables = true;
-    };
-    resumeDevice = "/dev/disk/by-label/nixos";
-  };
+  # Bootloader.
+  boot.loader.grub.enable = true;
+  boot.loader.grub.device = "/dev/vda";
+  boot.loader.grub.useOSProber = true;
 
   system.stateVersion = "23.11";
 }
