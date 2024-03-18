73bb579dc58c351fa98f42495a41aef6ca02bdab
Haseeb Majid <hello@haseebmajid.dev>
2024-03-18 19:11:11 +0000

    work

--- modules/homes/programs/discord/default.nix
+++ modules/homes/programs/discord/default.nix
@@ -3,7 +3,15 @@
 #  - Pull the script from sersorrel directly
 #  - Use python3.withPackages > writePython3Bin
 #  - Copy + alter discord's .desktop file
-{pkgs, ...}: let
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}:
+with lib;
+with lib.nixicle; let
+  cfg = config.programs.discord;
   patcher = pkgs.fetchurl {
     url = "https://raw.githubusercontent.com/sersorrel/sys/f7ff2fa325f786123a9b9e9a61b07be409dfb0b1/hm/discord/krisp-patcher.py";
     hash = "sha256-8AM6v+gR7VH0gXT0orqDSRyXaxv7eV4EnLum5FgW6Yk=";
@@ -23,6 +31,12 @@
     ${pkgs.gnused}/bin/sed 's!Exec=.*!Exec=${wrapperScript}!g' ${pkgs.discord}/share/applications/discord.desktop > $out/share/applications/discord.desktop
   '';
 in {
-  xdg.configFile."BetterDiscord/data/stable/custom.css" = {source = ./custom.css;};
-  home.packages = [wrappedDiscord];
+  options.programs.discord = with types; {
+    enable = mkBoolOpt false "Whether or not to manage discord";
+  };
+
+  config = mkIf cfg.enable {
+    xdg.configFile."BetterDiscord/data/stable/custom.css" = {source = ./custom.css;};
+    home.packages = [wrappedDiscord];
+  };
 }
