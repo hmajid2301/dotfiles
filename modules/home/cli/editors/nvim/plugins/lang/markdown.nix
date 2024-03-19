da5ab0de61c4723f1aac88070dff508fc8ed9fdf
Haseeb Majid <hello@haseebmajid.dev>
2024-03-19 23:05:25 +0000

    work work

--- modules/home/cli/editors/nvim/plugins/lang/markdown.nix
+++ modules/home/cli/editors/nvim/plugins/lang/markdown.nix
@@ -1,20 +1,4 @@
-{
-  config,
-  pkgs,
-  ...
-}: {
-  home.packages = with pkgs; [
-    marksman
-  ];
-
-  sops.secrets.languagetool_username = {
-    sopsFile = ../../../../../secrets.yaml;
-  };
-
-  sops.secrets.languagetool_api_key = {
-    sopsFile = ../../../../../secrets.yaml;
-  };
-
+{config, ...}: {
   programs.nixvim = {
     plugins = {
       image = {
