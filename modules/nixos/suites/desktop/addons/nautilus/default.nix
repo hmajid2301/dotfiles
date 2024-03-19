f170407cccacf328f002da57a808af7fce4f921d
Haseeb Majid <hello@haseebmajid.dev>
2024-03-19 03:55:54 +0000

    work

--- modules/nixos/suites/desktop/addons/nautilus/default.nix
+++ modules/nixos/suites/desktop/addons/nautilus/default.nix
@@ -13,15 +13,15 @@ in {
     enable = mkBoolOpt false "Whether to enable the gnome file manager.";
   };
 
-  dconf.settings = {
-    "org/gnome/desktop/privacy" = {
-      remember-recent-files = false;
-    };
-  };
-
   config = mkIf cfg.enable {
     services.gvfs.enable = true;
     services.udisks2.enable = true;
+
+    dconf.settings = {
+      "org/gnome/desktop/privacy" = {
+        remember-recent-files = false;
+      };
+    };
 
     environment.sessionVariables.GST_PLUGIN_SYSTEM_PATH_1_0 = lib.makeSearchPathOutput "lib" "lib/gstreamer-1.0" (with pkgs.gst_all_1; [
       gst-plugins-good
