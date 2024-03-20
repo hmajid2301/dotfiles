0ab28533258b3845cd1549490708d943c51f1c8f
Haseeb Majid <haseeb.majid@imaginecurve.com>
2024-03-20 14:41:22 +0000

    awork work

--- modules/nixos/services/virtualisation/kvm/default.nix
+++ modules/nixos/services/virtualisation/kvm/default.nix
@@ -7,7 +7,7 @@
 with lib;
 with lib.nixicle; let
   inherit (lib) types mkOption mkEnableOption optional optionals;
-  cfg = config.virtualisation.vfio;
+  cfg = config.virtualisation.kvm;
 
   tmpfileEntry = name: f: "f /dev/shm/${name} ${f.mode} ${f.user} ${f.group} -";
 
@@ -24,54 +24,7 @@ with lib.nixicle; let
     config.virtualisation.libvirtd.deviceACL;
 in {
   # Based on this https://gist.github.com/CRTified/43b7ce84cd238673f7f24652c85980b3
-  options.virtualisation = {
-    sharedMemoryFiles = mkOption {
-      type = types.attrsOf (types.submodule ({name, ...}: {
-        options = {
-          name = mkOption {
-            visible = false;
-            default = name;
-            type = types.str;
-          };
-          user = mkOption {
-            type = types.str;
-            default = "root";
-            description = "Owner of the memory file";
-          };
-          group = mkOption {
-            type = types.str;
-            default = "root";
-            description = "Group of the memory file";
-          };
-          mode = mkOption {
-            type = types.str;
-            default = "0600";
-            description = "Group of the memory file";
-          };
-        };
-      }));
-      default = {};
-    };
-    hugepages = {
-      enable = mkEnableOption "Hugepages";
-
-      defaultPageSize = mkOption {
-        type = types.strMatching "[0-9]*[kKmMgG]";
-        default = "1M";
-        description = "Default size of huge pages. You can use suffixes K, M, and G to specify KB, MB, and GB.";
-      };
-      pageSize = mkOption {
-        type = types.strMatching "[0-9]*[kKmMgG]";
-        default = "1M";
-        description = "Size of huge pages that are allocated at boot. You can use suffixes K, M, and G to specify KB, MB, and GB.";
-      };
-      numPages = mkOption {
-        type = types.ints.positive;
-        default = 1;
-        description = "Number of huge pages to allocate at boot.";
-      };
-    };
-
+  options.virtualisation.kvm = {
     libvirtd = {
       deviceACL = mkOption {
         type = types.listOf types.str;
@@ -112,6 +65,52 @@ in {
         default = false;
         example = true;
         description = "Enables or disables kvm guest access to model-specific registers";
+      };
+      sharedMemoryFiles = mkOption {
+        type = types.attrsOf (types.submodule ({name, ...}: {
+          options = {
+            name = mkOption {
+              visible = false;
+              default = name;
+              type = types.str;
+            };
+            user = mkOption {
+              type = types.str;
+              default = "root";
+              description = "Owner of the memory file";
+            };
+            group = mkOption {
+              type = types.str;
+              default = "root";
+              description = "Group of the memory file";
+            };
+            mode = mkOption {
+              type = types.str;
+              default = "0600";
+              description = "Group of the memory file";
+            };
+          };
+        }));
+        default = {};
+      };
+      hugepages = {
+        enable = mkEnableOption "Hugepages";
+
+        defaultPageSize = mkOption {
+          type = types.strMatching "[0-9]*[kKmMgG]";
+          default = "1M";
+          description = "Default size of huge pages. You can use suffixes K, M, and G to specify KB, MB, and GB.";
+        };
+        pageSize = mkOption {
+          type = types.strMatching "[0-9]*[kKmMgG]";
+          default = "1M";
+          description = "Size of huge pages that are allocated at boot. You can use suffixes K, M, and G to specify KB, MB, and GB.";
+        };
+        numPages = mkOption {
+          type = types.ints.positive;
+          default = 1;
+          description = "Number of huge pages to allocate at boot.";
+        };
       };
     };
   };
@@ -163,29 +162,43 @@ in {
       looking-glass-client
     ];
 
-    virtualisation.libvirtd.qemu = {
-      runAsRoot = false;
-      swtpm.enable = true;
-      ovmf = {
+    virtualisation = {
+      kvmgt.enable = true;
+      spiceUSBRedirection.enable = true;
+
+      libvirtd = {
         enable = true;
-        packages = [pkgs.OVMFFull.fd];
+        allowedBridges = [
+          "nm-bridge"
+          "virbr0"
+        ];
+        onBoot = "ignore";
+        onShutdown = "shutdown";
+        qemu = {
+          runAsRoot = false;
+          swtpm.enable = true;
+          ovmf = {
+            enable = true;
+            packages = [pkgs.OVMFFull.fd];
+          };
+
+          verbatimConfig = ''
+            clear_emulation_capabilities = ${
+              boolToZeroOne cfg.clearEmulationCapabilities
+            }
+            cgroup_device_acl = [
+              ${aclString}
+            ]
+          '';
+        };
+
+        services.udev.extraRules = ''
+          SUBSYSTEM=="vfio", OWNER="root", GROUP="kvm"
+        '';
+
+        systemd.tmpfiles.rules =
+          mapAttrsToList tmpfileEntry cfg.sharedMemoryFiles;
       };
-
-      verbatimConfig = ''
-        clear_emulation_capabilities = ${
-          boolToZeroOne cfg.clearEmulationCapabilities
-        }
-        cgroup_device_acl = [
-          ${aclString}
-        ]
-      '';
     };
-
-    services.udev.extraRules = ''
-      SUBSYSTEM=="vfio", OWNER="root", GROUP="kvm"
-    '';
-
-    systemd.tmpfiles.rules =
-      mapAttrsToList tmpfileEntry cfg.sharedMemoryFiles;
   };
 }
--- systems/x86_64-linux/desktop/default.nix
+++ systems/x86_64-linux/desktop/default.nix
@@ -5,42 +5,87 @@
 }: {
   imports = [
     ./hardware-configuration.nix
-
-    ../../nixos
-    ../../nixos/users/haseeb.nix
+    ./disks.nix
   ];
 
-  networking = {
-    hostName = "desktop";
-  };
-
-  virtualisation = {
-  };
-
-  modules.nixos = {
-    avahi.enable = true;
-    auto-hibernate.enable = false;
-    backup.enable = true;
-    bluetooth.enable = true;
-    docker.enable = true;
+  suites = {
     gaming.enable = true;
-    login.enable = true;
-    extraSecurity.enable = true;
-    power.enable = true;
-    virtualisation.enable = true;
-    vpn.enable = true;
+    desktop = {
+      enable = true;
+      addons = {
+        gnome.enable = true;
+        hyprland.enable = true;
+      };
+    };
   };
 
-  services.xserver.enable = true;
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
-  services.qemuGuest.enable = true;
-  services.spice-vdagentd.enable = true;
+  networking.hostName = "desktop";
 
-  # Bootloader.
-  boot.loader.grub.enable = true;
-  boot.loader.grub.device = "/dev/vda";
-  boot.loader.grub.useOSProber = true;
+  virtualisation.kvm = {
+    vfio = {
+      enable = true;
+      IOMMUType = "amd";
+      devices = ["10de:2208" "10de:1aef"];
+      blacklistNvidia = true;
+      sharedMemoryFiles = {
+        # scream = {
+        #   user = "haseeb";
+        #   group = "qemu-libvirtd";
+        #   mode = "666";
+        # };
+        looking-glass = {
+          user = "haseeb";
+          group = "haseeb";
+          mode = "666";
+        };
+      };
+      hugepages = {
+        enable = true;
+        defaultPageSize = "1G";
+        pageSize = "1G";
+        numPages = 16;
+      };
+    };
+    libvirtd = {
+      clearEmulationCapabilities = false;
+      deviceACL = [
+        "/dev/input/by-id/usb-Logitech_USB_Receiver-event-mouse"
+        "/dev/input/by-id/usb-ZSA_Technology_Labs_Voyager-event-kbd"
+        "/dev/vfio/vfio"
+        "/dev/vfio/2"
+        "/dev/vfio/6"
+        "dev/null"
+        "/dev/full"
+        "/dev/zero"
+        "/dev/random"
+        "/dev/urandom"
+        "/dev/ptmx"
+        "/dev/kvm"
+        "/dev/kqemu"
+        "/dev/rtc"
+        "/dev/hpet"
+        "/dev/kvm"
+        "/dev/shm/looking-glass"
+      ];
+    };
+  };
+
+  swapDevices = [{device = "/swap/swapfile";}];
+
+  boot = {
+    kernelParams = [
+      "resume_offset=533760"
+    ];
+    supportedFilesystems = lib.mkForce ["btrfs"];
+    kernelPackages = pkgs.linuxPackages_latest;
+    loader = {
+      systemd-boot.enable = true;
+      efi.canTouchEfiVariables = true;
+    };
+    resumeDevice = "/dev/disk/by-label/nixos";
+
+    initrd.systemd.enable = true;
+  };
 
   system.stateVersion = "23.11";
 }
