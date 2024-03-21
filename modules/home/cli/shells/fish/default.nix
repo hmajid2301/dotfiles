2d95bb7e54fa66a7acceebf59a2badd3ae4839cf
Haseeb Majid <hello@haseebmajid.dev>
2024-03-21 23:21:44 +0000

    owk work

--- modules/home/cli/shells/fish/default.nix
+++ modules/home/cli/shells/fish/default.nix
@@ -162,12 +162,13 @@ in {
           inherit (pkgs.fishPlugins.fzf-fish) src;
         }
         {
+          # TODO: until MR is merged in
           name = "nix.fish";
           src = pkgs.fetchFromGitHub {
-            owner = "kidonng";
+            owner = "Animeshz";
             repo = "nix.fish";
             rev = "ad57d970841ae4a24521b5b1a68121cf385ba71e";
-            sha256 = "13x3bfif906nszf4mgsqxfshnjcn6qm4qw1gv7nw89wi4cdp9i8q";
+            sha256 = "";
           };
         }
         {
