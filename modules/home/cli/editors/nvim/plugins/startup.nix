0c791b8df48f36245c39c38158b8f04885d58c70
Haseeb Majid <haseeb.majid@imaginecurve.com>
2024-03-22 10:14:05 +0000

    training

---
+++ modules/home/cli/editors/nvim/plugins/startup.nix
@@ -0,0 +1,30 @@
+{lib, ...}: {
+  programs.nixvim = {
+    plugins = {
+      auto-session = {
+        enable = true;
+        extraOptions = {
+          auto_save_enabled = true;
+          auto_restore_enabled = true;
+        };
+      };
+
+      mini = {
+        enable = true;
+        modules = {
+          starter = {
+            header = lib.concatLines [
+              "███╗   ██╗██╗██╗  ██╗██╗   ██╗██╗███╗   ███╗"
+              "████╗  ██║██║╚██╗██╔╝██║   ██║██║████╗ ████║"
+              "██╔██╗ ██║██║ ╚███╔╝ ██║   ██║██║██╔████╔██║"
+              "██║╚██╗██║██║ ██╔██╗ ╚██╗ ██╔╝██║██║╚██╔╝██║"
+              "██║ ╚████║██║██╔╝ ██╗ ╚████╔╝ ██║██║ ╚═╝ ██║"
+              "╚═╝  ╚═══╝╚═╝╚═╝  ╚═╝  ╚═══╝  ╚═╝╚═╝     ╚═╝"
+            ];
+            items = [];
+          };
+        };
+      };
+    };
+  };
+}
