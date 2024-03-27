9716a1c24064e364b6bfa8e9253ff548a9799ef6
Haseeb Majid <hello@haseebmajid.dev>
2024-03-27 04:12:03 +0000

    chore: Update Journal

--- modules/home/cli/editors/nvim/plugins/startup.nix
+++ modules/home/cli/editors/nvim/plugins/startup.nix
@@ -30,16 +30,11 @@
       local starter = require('mini.starter')
       local my_items = {
       	starter.sections.builtin_actions(),
-      	{ name = 'Sessions', action = ":lua require'telescope'.extensions.sessions.sessions{}", section = 'Telescope' },
       	{ name = 'Recent Files', action = ':Telescope oldfiles', section = 'Telescope' },
       	{ name = 'File Brower', action = ':Telescope file_browser', section = 'Telescope' },
       	-- starter.sections.recent_files(10, false),
       	-- starter.sections.recent_files(10, true),
       	-- Use this if you set up 'mini.sessions'
-      	starter.sections.sessions(9, true),
-      	{ name = 'Addons', action = ':e ~/.config/blender/2.82/scripts/addons', section = 'Bookmarks' },
-      	{ name = 'NvimPlugs', action = ':e ~/.local/share/nvim/site/pack/packer/start', section = 'Bookmarks' },
-
       }
       starter.setup({
       	-- Whether to open starter buffer on VimEnter. Not opened if Neovim was
