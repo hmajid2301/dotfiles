d28b268b5d6b2636ba370e45dca7738f07b681eb
Haseeb Majid <hello@haseebmajid.dev>
2024-08-28 00:03:45 +0100

    work

--- systems/x86_64-linux/vps/default.nix
+++ systems/x86_64-linux/vps/default.nix
@@ -12,29 +12,8 @@
   boot.loader.grub.enable = true;
   boot.loader.grub.device = "/dev/sda1";
 
-  services.openssh.enable = true;
-
-  users.users.nixos = {
-    isNormalUser = true;
-    extraGroups = ["wheel"];
-    initialHashedPassword = "$y$j9T$2DyEjQxPoIjTkt8zCoWl.0$3mHxH.fqkCgu53xa0vannyu4Cue3Q7xL4CrUhMxREKC"; # Password.123
-  };
-
-  programs.neovim = {
-    enable = true;
-    defaultEditor = true;
-    configure = {
-      customRC = ''
-        colorscheme habamax
-      '';
-
-      packages.packages = {
-        start = [
-          pkgs.vimPlugins.nerdtree
-        ];
-      };
-    };
-  };
+  roles.server.enable = true;
+  system.boot.enable = lib.mkForce false;
 
   system.stateVersion = "24.05";
 }
