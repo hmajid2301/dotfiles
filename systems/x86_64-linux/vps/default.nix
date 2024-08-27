ef30aa2d2475676849bf270f0ab9dc5e7dc706cb
Haseeb Majid <hello@haseebmajid.dev>
2024-08-28 00:07:56 +0100

    work

--- systems/x86_64-linux/vps/default.nix
+++ systems/x86_64-linux/vps/default.nix
@@ -15,5 +15,30 @@
   roles.server.enable = true;
   system.boot.enable = lib.mkForce false;
 
+  services = {
+    traefik = {
+      dynamicConfigOptions = {
+        http = {
+          services = {
+            jellyfin.loadBalancer.servers = [
+              {
+                url = "http://ms01:8096";
+              }
+            ];
+          };
+
+          routers = {
+            homeassistant = {
+              entryPoints = ["websecure"];
+              rule = "Host(`jellyfin.haseebmajid.dev`)";
+              service = "jellyfin";
+              tls.certResolver = "letsencrypt";
+            };
+          };
+        };
+      };
+    };
+  };
+
   system.stateVersion = "24.05";
 }
