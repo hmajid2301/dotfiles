612d08b59f3fbaa91277f9de0167733e34a1408d
Haseeb Majid <haseeb.majid@imaginecurve.com>
2024-09-04 17:51:06 +0100

    work

--- modules/nixos/services/jellyfin/default.nix
+++ modules/nixos/services/jellyfin/default.nix
@@ -43,6 +43,13 @@ in {
             };
 
             routers = {
+              jellyfin-vps = {
+                entryPoints = ["websecure"];
+                rule = "Host(`jellyfin.haseebmajid.dev`)";
+                service = "jellyfin";
+                tls.certResolver = "letsencrypt";
+              };
+
               jellyfin = {
                 entryPoints = ["websecure"];
                 rule = "Host(`jellyfin.homelab.haseebmajid.dev`)";
